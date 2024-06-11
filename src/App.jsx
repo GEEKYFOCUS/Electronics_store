4f888d1f2d02037d8231ed9a77a7b3d1ccd4076b
geekyfocus <gidabdulharkiem27.com>
2024-06-11 09:56:26 +0100

    redux toolkit configured

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,32 @@ import Category from "./pages/Category";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import PageNotFound from "./pages/PageNotFound";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="homepage" />} />
-          <Route path="homepage" element={<HomePage />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="checkout/:orderId" element={<Checkout />} />
-          <Route path="carts" element={<Carts />} />
-          <Route path="account" element={<Account />} />
-          <Route path="product/:id" element={<Product />} />
-          <Route path="wishlist" element={<WishList />} />
-          <Route path="category" element={<Category />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="homepage" />} />
+            <Route path="homepage" element={<HomePage />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="checkout/:orderId" element={<Checkout />} />
+            <Route path="carts" element={<Carts />} />
+            <Route path="account" element={<Account />} />
+            <Route path="product/:id" element={<Product />} />
+            <Route path="wishlist" element={<WishList />} />
+            <Route path="category" element={<Category />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
